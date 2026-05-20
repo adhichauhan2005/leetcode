adfcdfa3687a01c149bce2a5c37ce7ba81278403
adhichauhan2005 <adhirajsinghchauhan16@gmail.com>
2026-05-20 17:35:53 +0530

    Create 12.py

---
+++ DSA/12.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def intToRoman(self, num):
+        values = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        ]
+
+        result = ""
+        # 58
+        # 58>50 --> L
+        # Remaining 8 --> 8>5 --> V
+        # add 3 I
+        for value, symbol in values:
+            while num >= value:
+                result += symbol
+                num -= value
+
+        return result
