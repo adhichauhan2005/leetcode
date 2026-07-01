efdfecef282f17c69b5893a5471eb41cdf4c2b5b
Adhiraj Singh Chauhan <adhirajsinghchauhan16@gmail.com>
2026-07-01 15:05:48 +0530

    Create 47.py

---
+++ DSA/47.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        nums.sort()
+        res = []
+        path = []
+        used = [False] * len(nums)
+
+        def backtrack():
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+            for i in range(len(nums)):
+
+                if used[i]:
+                    continue
+
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+
+                path.append(nums[i])
+                used[i] = True
+
+                backtrack()
+
+                path.pop()
+                used[i] = False
+
+        backtrack()
+        return res
